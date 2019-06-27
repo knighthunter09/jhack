b9d01d43982285e53b341af8330c2868fcd3ea09
knighthunter09 <shekhars0920@gmail.com>
2019-06-27 13:35:46 +0800

    First

--- rest_api.py
+++ rest_api.py
@@ -1,8 +1,10 @@
 import json
 import pickle
+import re
 
 from flask import Flask, jsonify, request,Response
 from flask_restful import reqparse, abort, Api, Resource
+from textblob import TextBlob
 
 with open('naive_bayes_classifier.pickle', 'rb') as f:
     classifier = pickle.load(f)
@@ -36,6 +38,23 @@ class Predictor(Resource):
 
 api.add_resource(Predictor, '/nbpredict')
 
+class PredictorTB(Resource):
+    def post(self):
+        json_data = request.get_json(force=True)
+
+        result = []
+        for tweetData in json_data:
+            tweet = tweetData['tweet']
+            date = tweetData['date']
+
+            tweetMatrix = analyze_sentiment(tweet)
+            res = {"date":date, "sentiment":tweetMatrix}
+            result.append(res)
+            return result
+
+        return Response(json.dumps(result),  mimetype='application/json')
+api.add_resource(PredictorTB, '/tbpredict')
+
 def naiveBayesSentimentCalculator(review):
   problemInstance = review.split()
   problemFeatures = extract_features(problemInstance)
@@ -48,6 +67,18 @@ def extract_features(review):
       features[word]=(word in review_words)
   return features
 
+def clean_tweet(text):
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", text).split())
+
+def analyze_sentiment(text):
+    analysis = TextBlob(clean_tweet(text))
+
+    if analysis.sentiment.polarity > 0:
+        return 1
+    elif analysis.sentiment.polarity == 0:
+        return 0
+    else:
+        return -1
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5445 ,debug=True)
